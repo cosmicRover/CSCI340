3e0de92dc0486bae654eb33eed34d16927e8f2c0
cosmicRover <jpaul3250@gmail.com>
2020-02-28 12:01:17 -0500

    bit more progress

--- time.cpp
+++ time.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
 #include <string>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctime>
 
 //a macro to help print text and var
 #define print(e, f)                       \
     {                                     \
         std::cout << e << f << std::endl; \
     };
+
+double getTimeOfDay(){
+    time_t now = time(0);
+    return now;
+}
 
 void executeCmd(std::string s)
 {
@@ -20,19 +27,24 @@ void executeCmd(std::string s)
 int main(int argc, char *argv[])
 {
     std::string c = argv[1]; 
-    std::string cmd = "/bin/" + c ;
+    std::string cmd = "/bin/"+c;
     print("command entered: ", cmd);
 
-    // char cmd_char[cmd.length() + 1];
-    // for (size_t i = 0; i < cmd.length(); i++)
-    // {
-    //     cmd_char[i] = cmd[i];
-    // }
-    
+    char cmd_char[cmd.length()];
+    for (int i = 0; i <= cmd.length(); i++)
+    {
+        cmd_char[i] = cmd[i];
 
+    }
+
+    //debug
+    // print("cmd_char is: ", cmd_char);
+
+    
     int pipes[2];
     pid_t pid;
     char cmdOutput[4096];
+    float pStart, cStart, pEnd;
 
     //create a pipe
     if (pipe(pipes) == -1)
@@ -51,19 +63,30 @@ int main(int argc, char *argv[])
     if (pid == 0)
     {
         print("child process was created ", getppid());
+        cStart = getTimeOfDay();
         dup2(pipes[1], STDOUT_FILENO);
         close(pipes[0]);
         close(pipes[1]);
-        execl("/bin/ls", (char *)0);
+        execl(cmd_char, "", (char *) 0);
+    }
+    else if(pid < 0){
+        print("child process was killed", getppid());
     }
     else
     {
         print("parent process ", getpid());
+        pStart = getTimeOfDay();
         close(pipes[1]);
         int bytes = read(pipes[0], cmdOutput, sizeof(cmdOutput));
         printf("outputs are --> (%.*s)\n", bytes, cmdOutput);
-        wait(NULL);
+        wait(0);
+        pEnd = getTimeOfDay();
     }
+
+    print("child started: ", cStart);
+    print("parent started: ", pStart);
+    print("parent stopped: ", pEnd);
+    print("elapsed time: ", pEnd - pStart);
 
     return 0;
 }
