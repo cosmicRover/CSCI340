9d284d243dd37337fe80b233418ce964de36e6ab
joypaul <jpaul3250@gmail.com>
2020-03-11 22:33:30 -0400

    project2

---
+++ project2/project2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+/*
+specs:
+1) generate 10k random numbers from 1-10,000
+2) sort using an nlogn algo then output the min, max from the sample
+3) find the min of the 10k samples using non MT way
+4) find the min using MT way. (10,000/5) => 2,000/5 => 400/5 => 80 per thread. Could even stop at 400
+*/
+
+
+int main(){
+    
+    return 0;
+}
