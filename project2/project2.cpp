ca84837643448b46f239d9921a24181cc09c4d53
joypaul <jpaul3250@gmail.com>
2020-03-17 00:36:12 -0400

    first get min done

--- project2/project2.cpp
+++ project2/project2.cpp
@@ -7,106 +7,64 @@
 specs:
 1) generate 10k random numbers from 1-10,000 [x]
 2) sort using and then output the min, max from the sample [x]
-3) find the min of the 10k samples using non MT way. Perhaps craft mergeSort [in-progress]
+3) find the min of the 10k samples using non MT way. P̶e̶r̶h̶a̶p̶s̶ ̶c̶r̶a̶f̶t̶ ̶m̶e̶r̶g̶e̶S̶o̶r̶t̶ Linear pass[x]
 4) find the min using MT way. (10,000/5) => 2,000/5 => 400/5 => 80 per thread. Could even stop at 400
 */
 
 #define v_int std::vector<int> //macro for int vector declaration
 
-void displaySample(v_int sample){
+void displaySample(v_int sample)
+{
     for (size_t i = 0; i < sample.size(); i++)
     {
         printf("%d, ", sample[i]);
     }
 }
 
-v_int getSample(){
+/*get random numbers up to a million*/
+v_int getSample()
+{
     int SIZE = 100;
     v_int sample;
-    srand((unsigned) time(0));
-    
+    srand((unsigned)time(0));
+
     for (size_t i = 0; i < SIZE; i++)
     {
-        sample.push_back(rand() % SIZE);
+        sample.push_back(rand() % 100000);
     }
-    
+
     return sample;
 }
 
-void initialMinMax(v_int sample){
+void initialMinMax(v_int sample)
+{
     sort(sample.begin(), sample.end());
-    printf("\nmin is %d, max is %d\n", sample[0], sample[sample.size() -1]);
+    printf("\nmin is %d, max is %d\n", sample[0], sample[sample.size() - 1]);
 }
 
-void merge(v_int array, int l, int m, int r){
-    int i, j, k;
-    int n1 = m-1 +1;
-    int n2 = r - m;
-
-    /*temp array to hold data */
-    int L[n1], R[n2];
-
-    for (i = 0; i < n1; i++)
-        L[i] = array[l + i];
-    for (j = 0; j < n2; j++)
-        R[j] = array[m + 1 + j];
-
-    /*Merge back to main array*/
-    i = 0;
-    j = 0;
-    k = 1;
-    
-    while (i < n1 && j < n2)
+/*get min in linear time */
+int getMin(v_int arr)
+{
+    int min = 2147483647;
+    for (size_t i = 0; i < arr.size(); i++)
     {
-        if(L[i] <= R[i]){
-            array[k] = L[i];
-            i++;
-        }else
+        if (arr[i] < min)
         {
-            array[k] = R[j];
-            j++;
+            min = arr[i];
         }
-        k++;
-    }
-    
-    while (i<n1)
-    {
-        array[k] = L[i];
-        i++;
-        k++;
     }
 
-    while (j<n2)
-    {
-        array[k] = R[j];
-        j++;
-        k++;
-    }
+    return min;
 }
 
-void mergeSort(v_int array, int l, int r){
-    if (l<r){
-        int m = l+(r-1)/2;
-
-        mergeSort(array, l, m);
-        mergeSort(array, m+1, r);
-
-        merge(array, l, m, r);
-    }
-}
-
-    int main()
+int main()
 {
     v_int sample = getSample();
     // displaySample(sample);
     initialMinMax(sample);
-
-    mergeSort(sample, 0, sample.size() - 1);
-    // for (size_t i = 0; i < sample.size(); i++)
-    // {
-    //     printf("%d ", sample[i]);
-    // }
-    
+    int nonMtMin = getMin(sample);
+    printf("Min from nonMt is: %d\n", nonMtMin);
+    /*TODO get min the MT way*/
 
     return 0;
 }
